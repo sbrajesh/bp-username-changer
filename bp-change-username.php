53c6718cfdc63052be045535430d4c6df9d68694
r-a-y <r-a-y@gmx.com>
2014-11-12 17:16:30 -0800

    Trim whitespace.

--- bp-change-username.php
+++ bp-change-username.php
@@ -23,7 +23,7 @@ define( 'BPCU_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
  */
 
 function bpdev_bpcu_load_textdomain() {
-	
+
 	$locale = apply_filters( 'bpdev_bpcu_load_textdomain_get_locale', get_locale() );
 	// if load .mo file
 
@@ -43,19 +43,19 @@ add_action( 'bp_init', 'bpdev_bpcu_load_textdomain', 2 );
 
 /**
  * Add sub menu to the User settings page
- * 
+ *
  * @return type
  */
 function bpdev_bpcu_nav_setup() {
-	
+
 	//only add if settings component is enabled
 	if( !bp_is_active( 'settings' ) )
 		return;
-	
+
 	$bp = buddypress();
-	
+
 	$settings_link = bp_displayed_user_domain() . bp_get_settings_slug() . '/';
-	
+
 	bp_core_new_subnav_item( array(
 		'name' => __( 'Change Username', 'bpcu' ),
 		'slug' => BPCU_SLUG,
@@ -71,22 +71,21 @@ add_action('bp_setup_nav', 'bpdev_bpcu_nav_setup', 11);
 
 /**
  * Show/Update Username
- * 
+ *
  * @global type $wpdb
  * @global type $bp
- * @global null $current_user
  * @return type
  */
 function bpdev_bpcu_settings_screen() {
-	
-	global $wpdb, $bp, $current_user;
+
+	global $wpdb, $bp;
 
 	$error          = false;
 	$is_super_admin = false;
 	$user_id        = bp_displayed_user_id();
 
 	if ( isset( $_POST['bpcu_change_username_submit' ] ) ) {
-		
+
 		//check_admin_referer('bp_settings_change_username');
 
 		$new_user_name = $_POST['bpcu_new_user_name'];
@@ -95,38 +94,38 @@ function bpdev_bpcu_settings_screen() {
 		// it before using the function
 		wp_cache_delete( $new_user_name, 'userlogins' );
 		wp_cache_delete( $_POST['bpcu_current_user_name'], 'userlogins' );
-		
+
 		//if the username is empty or invalid
 		if ( empty( $new_user_name ) || !validate_username( $new_user_name ) ) {
-			
+
 			$error = true;
 			$message = __( 'Please enter a valid Username!', 'bpcu' );
-			
+
 		} elseif ( ! $error && $bp->displayed_user->userdata->user_login == $new_user_name ) {
 			//if the provided name is same as the current username
 			$error = true;
 			$message = __( 'Please enter a differnt Username!', 'bpcu' );
-			
+
 		}elseif ( ! $error && username_exists( $new_user_name ) ) {
-			//else if the username already exists 
+			//else if the username already exists
 			$error	 = true;
 			$message = sprintf( __( 'The Username %s already exists. Please use a different username!', 'bpcu' ), $new_user_name );
-			
+
 		} elseif ( ! $error && bpdev_bpcu_is_reserved_name( $new_user_name ) ) {
-			
+
 			$error	 = true;
 			$message = sprintf( __( 'The Username %s is reserved. Please choose a differenet username!' ), $new_user_name );
-			
+
 		}
-		
+
 		//if there was an error
 		//show error &redirect
 		if ( $error ) {
-			
+
 			bp_core_add_message( $message, 'error' );
-			
+
 			bp_core_redirect( bp_displayed_user_domain() . $bp->settings->slug . '/' . BPCU_SLUG . '/' );
-			
+
 			return;//we don't need this return anyway
 		}
 
@@ -137,7 +136,7 @@ function bpdev_bpcu_settings_screen() {
 
 		//if it is multisite, before change the username, revoke the admin capability
 		if( is_multisite() && is_super_admin( $user_id ) ) {
-			
+
 			if( ! function_exists( 'revoke_super_admin' ) ) {
 				require_once( ABSPATH . 'wp-admin/includes/ms.php' );
 			}
@@ -155,7 +154,7 @@ function bpdev_bpcu_settings_screen() {
 		wp_cache_delete( 'bp_core_userdata_' . $user_id, 'bp' );
 		wp_cache_delete( 'bp_user_username_' . $user_id, 'bp' );
 		wp_cache_delete( 'bp_user_domain_' . $user_id, 'bp' );
-				
+
 		//reset auth cookie for new user_login
 		wp_set_auth_cookie( $user_id, true, false );
 		//force to reset the global $current_user with the new user details
@@ -163,20 +162,20 @@ function bpdev_bpcu_settings_screen() {
 		wp_set_current_user( $user_id ); //reset user
 		$user = wp_get_current_user();
 		//if multisite and the user was super admin, mark him back as super admin
-		
+
 		if( is_multisite() && $is_super_admin ){
 			grant_super_admin( $user_id );
 		}
 
 
-		
+
 
 		bp_core_add_message( __( 'Username Changed Successfully!', 'bpcu' ) );
 
 		do_action( 'profile_update', $user_id, $old_user_data );
-		
+
 		bp_core_redirect( bp_displayed_user_domain() . $bp->settings->slug . '/' . BPCU_SLUG . '/' );
-		
+
 		return;
 	}
 	//show title &form
@@ -188,14 +187,14 @@ function bpdev_bpcu_settings_screen() {
 
 /**
  * Change Username form
- * 
+ *
  * @global null $current_user
  */
 function bpdev_bpcu_form() {
 	$bp = buddypress();
 	?>
 	<form name="bpcu_username_changer" method="post" class="standard-form">
-		
+
 		<label for="bpcu_current_user_name"><?php _e("Current User name", "bpcu") ?></label>
 		<input type="text" name="bpcu_current_user_name" id="bpcu_current_user_name" value="<?php echo esc_attr( $bp->displayed_user->userdata->user_login ); ?>" class="settings-input"  disabled="disabled"/>
 
@@ -204,49 +203,49 @@ function bpdev_bpcu_form() {
 
 		<p><?php _e("Enter the new Username of your choice", "bpcu") ?></p>
 		<p class="submit"><input type="submit" id="bpcu_change_username_submit" name="bpcu_change_username_submit" class="button" value="<?php _e('Save Changes', 'bpcu') ?>" /></p>
-	
+
 	</form>
 
 	<?php
 }
 
 /**
- * Settings content title 
+ * Settings content title
  */
 function bpdev_bpcu_title() {
-	
+
 	echo '<h3>' . __( 'Change Username', 'bpcu' ) . '</h3>';
 }
 
 /**
  * Check if Username is reserved
- * 
+ *
  * @param type $username
  * @return boolean
  */
 function bpdev_bpcu_is_reserved_name( $username ) {
-	
+
 	$reserved = array();
-	
+
 	$admin_names = array( 'admin', 'administrator' );
-	
+
 	if ( is_super_admin() && in_array( $username, $admin_names ) )
 		return false; //do not prohibit the super admin from any username
-		
-//other than that, check for all illigal names 
+
+//other than that, check for all illigal names
 	if ( function_exists( 'bp_core_get_illegal_names' ) ) {
-		
+
 		$reserved = bp_core_get_illegal_names();
-	
-		
+
+
 	}elseif ( function_exists( 'bp_core_illegal_names' ) ) {
-		
+
 		$reserved = bp_core_illegal_names();
-		
+
 	}
-	
+
 	if ( in_array( $username, (array) $reserved ) )
 		return true;
-	
+
 	return false;
 }
