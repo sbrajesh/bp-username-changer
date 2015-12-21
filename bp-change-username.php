0960af20b01435b94bdeff11292093bfa0a1c13f
ravi <ravivats89@gmail.com>
2015-12-22 00:40:27 +0530

    Refactoring the code

--- bp-change-username.php
+++ bp-change-username.php
@@ -16,254 +16,294 @@
 if ( !defined( 'BPCU_SLUG' ) )
 	define( 'BPCU_SLUG', 'change-username' );
 
-define( 'BPCU_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+class BP_Username_Change_Helper {
+    
+    private static $instance = NULL;
+    
+    private function __construct() {
+        
+        $this->setup();
+    
+    }
+    
+    public static function get_instance() {
+        
+        if ( is_null( self::$instance ) ) {
+            
+            self::$instance = new self();
+            
+        }
+        
+        return self::$instance;
+        
+    }
+    
+    private function setup() {
+        
+        add_action( 'bp_init', array( $this, 'load_textdomain' ) );
+        add_action( 'bp_setup_nav', array( $this, 'nav_setup' ), 11 );
+        
+    }
+    
+    /**
+    * Load Translation
+    */
+    public function load_textdomain() {
 
-/**
- * Load Translation
- */
-
-function bpdev_bpcu_load_textdomain() {
-
-	$locale = apply_filters( 'bpdev_bpcu_load_textdomain_get_locale', get_locale() );
-	// if load .mo file
-
-	if ( !empty( $locale ) ) {
-		$mofile_default = sprintf( '%slanguages/%s.mo', BPCU_PLUGIN_DIR, $locale );
-
-		$mofile = apply_filters( 'bpdev_bpcu_load_textdomain_mofile', $mofile_default );
-
-		if ( file_exists( $mofile ) ) {
-			// make sure file exists, and load it
-			load_textdomain( 'bpcu', $mofile );
-		}
-	}
-}
-
-add_action( 'bp_init', 'bpdev_bpcu_load_textdomain', 2 );
-
-/**
- * Add sub menu to the User settings page
- *
- * @return type
- */
-function bpdev_bpcu_nav_setup() {
+	load_plugin_textdomain( 'bp-username-changer', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+        
+    }
+    
+    /**
+    * Add sub menu to the User settings page
+    *
+    * @return type
+    */
+    public function nav_setup() {
 
 	//only add if settings component is enabled
-	if( !bp_is_active( 'settings' ) )
-		return;
-
+        if ( ! bp_is_active( 'settings' ) ) {
+            
+            return;
+            
+        }
+		
 	$bp = buddypress();
-
-	$settings_link = bp_displayed_user_domain() . bp_get_settings_slug() . '/';
-
+        $settings_link = bp_displayed_user_domain() . bp_get_settings_slug() . '/';
+        
 	bp_core_new_subnav_item( array(
-		'name' => __( 'Change Username', 'bpcu' ),
-		'slug' => BPCU_SLUG,
-		'parent_url' => $settings_link,
-		'parent_slug' => $bp->settings->slug,
-		'screen_function' => 'bpdev_bpcu_settings_screen',
-		'position' => 30,
-		'user_has_access' => apply_filters( 'bpcu_user_has_access', bp_is_my_profile() || is_super_admin() ),
-	) );
-}
+                'name'              => __( 'Change Username', 'bp-username-changer' ),
+		'slug'              => BPCU_SLUG,
+		'parent_url'        => $settings_link,
+		'parent_slug'       => $bp->settings->slug,
+		'screen_function'   => array( $this,'settings_screen'),
+		'position'          => 30,
+		'user_has_access'   => apply_filters( 'bpcu_user_has_access', bp_is_my_profile() || is_super_admin() ),
+            )
+        );
+         
+    }
 
-add_action('bp_setup_nav', 'bpdev_bpcu_nav_setup', 11);
+    /**
+    * Show/Update Username
+    *
+    * @global type $wpdb
+    * @return type
+    */
+    public function settings_screen() {
 
-/**
- * Show/Update Username
- *
- * @global type $wpdb
- * @global type $bp
- * @return type
- */
-function bpdev_bpcu_settings_screen() {
-
-	global $wpdb, $bp;
+	global $wpdb;
+        $bp = buddypress();
 
 	$error          = false;
 	$is_super_admin = false;
 	$user_id        = bp_displayed_user_id();
 
-	if ( isset( $_POST['bpcu_change_username_submit' ] ) ) {
+	if ( isset( $_POST['change_username_submit'] ) ) {
 
-		//check_admin_referer('bp_settings_change_username');
+            //check_admin_referer('bp_settings_change_username');
+            $new_user_name = $_POST['new_user_name'];
 
-		$new_user_name = $_POST['bpcu_new_user_name'];
+            // username_exists() references the userlogins object cache, so we must clear
+            // it before using the function
+            wp_cache_delete( $new_user_name, 'userlogins' );
+            wp_cache_delete( $_POST['current_user_name'], 'userlogins' );
 
-		// username_exists() references the userlogins object cache, so we must clear
-		// it before using the function
-		wp_cache_delete( $new_user_name, 'userlogins' );
-		wp_cache_delete( $_POST['bpcu_current_user_name'], 'userlogins' );
+            //if the username is empty or invalid
+            if ( empty( $new_user_name ) || !validate_username( $new_user_name ) ) {
 
-		//if the username is empty or invalid
-		if ( empty( $new_user_name ) || !validate_username( $new_user_name ) ) {
+		$error = true;
+		$message = __( 'Please enter a valid Username!', 'bp-username-changer' );
 
-			$error = true;
-			$message = __( 'Please enter a valid Username!', 'bpcu' );
+            } elseif ( ! $error && $bp->displayed_user->userdata->user_login == $new_user_name ) {
+		
+                //if the provided name is same as the current username
+		$error = true;
+		$message = __( 'Please enter a differnt Username!', 'bp-username-changer' );
 
-		} elseif ( ! $error && $bp->displayed_user->userdata->user_login == $new_user_name ) {
-			//if the provided name is same as the current username
-			$error = true;
-			$message = __( 'Please enter a differnt Username!', 'bpcu' );
+            } elseif ( ! $error && username_exists( $new_user_name ) ) {
+		
+                //else if the username already exists
+		$error	 = true;
+		$message = sprintf( __( 'The Username %s already exists. Please use a different username!', 'bp-username-changer' ), $new_user_name );
 
-		}elseif ( ! $error && username_exists( $new_user_name ) ) {
-			//else if the username already exists
-			$error	 = true;
-			$message = sprintf( __( 'The Username %s already exists. Please use a different username!', 'bpcu' ), $new_user_name );
+            } elseif ( ! $error && $this->is_reserved_name( $new_user_name ) )  {
 
-		} elseif ( ! $error && bpdev_bpcu_is_reserved_name( $new_user_name ) ) {
+		$error	 = true;
+		$message = sprintf( __( 'The Username %s is reserved. Please choose a differenet username!' ), $new_user_name );
 
-			$error	 = true;
-			$message = sprintf( __( 'The Username %s is reserved. Please choose a differenet username!' ), $new_user_name );
+            }
 
-		}
+            //if there was an error
+            //show error &redirect
+            if ( $error ) {
 
-		//if there was an error
-		//show error &redirect
-		if ( $error ) {
+		bp_core_add_message( $message, 'error' );
 
-			bp_core_add_message( $message, 'error' );
+		bp_core_redirect( bp_displayed_user_domain() . $bp->settings->slug . '/' . BPCU_SLUG . '/' );
 
-			bp_core_redirect( bp_displayed_user_domain() . $bp->settings->slug . '/' . BPCU_SLUG . '/' );
+		return;//we don't need this return anyway
+                
+            }
 
-			return;//we don't need this return anyway
-		}
+            //if it is multisite, before change the username, revoke the admin capability
+            if( is_multisite() && is_super_admin( $user_id ) ) {
 
-		//if it is multisite, before change the username, revoke the admin capability
-		if( is_multisite() && is_super_admin( $user_id ) ) {
+		if( ! function_exists( 'revoke_super_admin' ) ) {
+	
+                    require_once( ABSPATH . 'wp-admin/includes/ms.php' );
+		
+                }
 
-			if( ! function_exists( 'revoke_super_admin' ) ) {
-				require_once( ABSPATH . 'wp-admin/includes/ms.php' );
-			}
+		$is_super_admin = true;
+		
+                revoke_super_admin( $user_id );
+		    
+            }
 
-			$is_super_admin = true;
-			revoke_super_admin( $user_id );
-		}
+            /* now, update the user_login / user_nicename in the database */
+            // this will update user_nicename
+            // wp_update_user() doesn't update user_login when updating a user... sucks!
+            wp_update_user( array(
+                
+                'ID'            => $user_id,
+            	'user_login'    => $new_user_name,
+		'user_nicename' => sanitize_title( $new_user_name )
+                
+            ) );
 
-		/* now, update the user_login / user_nicename in the database */
+            // manually update user_login
+            $wpdb->update( $wpdb->users, array( 'user_login' => $new_user_name ), array( 'ID' => $user_id ), array( '%s' ), array( '%d' ) );
 
-		// this will update user_nicename
-		// wp_update_user() doesn't update user_login when updating a user... sucks!
-		wp_update_user( array(
-			'ID' => $user_id,
-			'user_login' => $new_user_name,
-			'user_nicename' => sanitize_title( $new_user_name )
-		) );
+            // delete object cache
+            clean_user_cache( $user_id );
+            wp_cache_delete( $user_id, 'users' );
+            wp_cache_delete( 'bp_core_userdata_' . $user_id, 'bp' );
+            wp_cache_delete( 'bp_user_username_' . $user_id, 'bp' );
+            wp_cache_delete( 'bp_user_domain_' . $user_id, 'bp' );
 
-		// manually update user_login
-		$wpdb->update( $wpdb->users, array( 'user_login' => $new_user_name ), array( 'ID' => $user_id ), array( '%s' ), array( '%d' ) );
+            // reset auth cookie for new user_login
+            // only do this if the current user is attempting to change their own username
+            // copies auth cookie logic from wp_update_user()
+            if ( bp_is_my_profile() ) {
+                
+		wp_clear_auth_cookie();
 
-		// delete object cache
-		clean_user_cache( $user_id );
-		wp_cache_delete( $user_id, 'users' );
-		wp_cache_delete( 'bp_core_userdata_' . $user_id, 'bp' );
-		wp_cache_delete( 'bp_user_username_' . $user_id, 'bp' );
-		wp_cache_delete( 'bp_user_domain_' . $user_id, 'bp' );
+		// Here we calculate the expiration length of the current auth cookie and compare it to the default expiration.
+		// If it's greater than this, then we know the user checked 'Remember Me' when they logged in.
+		$logged_in_cookie    = wp_parse_auth_cookie( '', 'logged_in' );
+		
+                /** This filter is documented in wp-includes/pluggable.php */
+		$default_cookie_life = apply_filters( 'auth_cookie_expiration', ( 2 * DAY_IN_SECONDS ), $user_id, false );
+		$remember            = ( ( $logged_in_cookie['expiration'] - time() ) > $default_cookie_life );
 
-		// reset auth cookie for new user_login
-		// only do this if the current user is attempting to change their own username
-		// copies auth cookie logic from wp_update_user()
-		if ( bp_is_my_profile() ) {
-			wp_clear_auth_cookie();
+		wp_set_auth_cookie( $user_id, $remember );
+		                
+            }
 
-			// Here we calculate the expiration length of the current auth cookie and compare it to the default expiration.
-			// If it's greater than this, then we know the user checked 'Remember Me' when they logged in.
-			$logged_in_cookie    = wp_parse_auth_cookie( '', 'logged_in' );
-			/** This filter is documented in wp-includes/pluggable.php */
-			$default_cookie_life = apply_filters( 'auth_cookie_expiration', ( 2 * DAY_IN_SECONDS ), $user_id, false );
-			$remember            = ( ( $logged_in_cookie['expiration'] - time() ) > $default_cookie_life );
+            //if multisite and the user was super admin, mark him back as super admin
+            if( is_multisite() && $is_super_admin ){
+	
+                grant_super_admin( $user_id );
+		    
+            }
 
-			wp_set_auth_cookie( $user_id, $remember );
-		}
+            // add message
+            bp_core_add_message( __( 'Username Changed Successfully!', 'bp-username-changer' ) );
 
-		//if multisite and the user was super admin, mark him back as super admin
-		if( is_multisite() && $is_super_admin ){
-			grant_super_admin( $user_id );
-		}
+            // fetch the user object just in case plugins altered the user_login
+            $user = new WP_User( $user_id );
 
-		// add message
-		bp_core_add_message( __( 'Username Changed Successfully!', 'bpcu' ) );
+            // hook for plugins
+            do_action( 'bp_username_changed', $new_user_name, $bp->displayed_user->userdata, $user );
 
-		// fetch the user object just in case plugins altered the user_login
-		$user = new WP_User( $user_id );
+            // redirect
+            // bp_core_get_user_domain() requires the new user_nicename / user_login
+            bp_core_redirect( bp_core_get_user_domain( $user_id, $user->user_nicename, $user->user_login ) . $bp->settings->slug . '/' . BPCU_SLUG . '/' );
 
-		// hook for plugins
-		do_action( 'bp_username_changed', $new_user_name, $bp->displayed_user->userdata, $user );
+            return;
+            
+        }
+        
+        //show title &form
+	add_action( 'bp_template_title', array( $this, 'print_title' ) );
+	add_action( 'bp_template_content', array( $this, 'print_form' ) );
+        bp_core_load_template( apply_filters( 'bp_cu_template_settings', 'members/single/plugins' ) );
+    
+    }
+    
+    /**
+    * Change Username form
+    *
+    * @global null $current_user
+    */
+    public function print_form() {
+	
+        $bp = buddypress();
+    
+    ?>
+	<form name="username_changer" method="post" class="standard-form">
 
-		// redirect
-		// bp_core_get_user_domain() requires the new user_nicename / user_login
-		bp_core_redirect( bp_core_get_user_domain( $user_id, $user->user_nicename, $user->user_login ) . $bp->settings->slug . '/' . BPCU_SLUG . '/' );
-
-		return;
-	}
-	//show title &form
-	add_action( 'bp_template_title', 'bpdev_bpcu_title' );
-	add_action( 'bp_template_content', 'bpdev_bpcu_form' );
-
-	bp_core_load_template( apply_filters( 'bp_cu_template_settings', 'members/single/plugins' ) );
-}
-
-/**
- * Change Username form
- *
- * @global null $current_user
- */
-function bpdev_bpcu_form() {
-	$bp = buddypress();
-	?>
-	<form name="bpcu_username_changer" method="post" class="standard-form">
-
-		<label for="bpcu_current_user_name"><?php _e("Current User name", "bpcu") ?></label>
-		<input type="text" name="bpcu_current_user_name" id="bpcu_current_user_name" value="<?php echo esc_attr( $bp->displayed_user->userdata->user_login ); ?>" class="settings-input"  disabled="disabled"/>
-
-		<label for="new_user_name"><?php _e("New User name", "bpcu") ?></label>
-		<input type="text" name="bpcu_new_user_name" id="bpcu_new_user_name" value="" class="settings-input" />
-
-		<p><?php _e("Enter the new Username of your choice", "bpcu") ?></p>
-		<p class="submit"><input type="submit" id="bpcu_change_username_submit" name="bpcu_change_username_submit" class="button" value="<?php _e('Save Changes', 'bpcu') ?>" /></p>
+            <label for="current_user_name"><?php _e("Current User name", "bp-username-changer") ?></label>
+            <input type="text" name="current_user_name" id="current_user_name" value="<?php echo esc_attr( $bp->displayed_user->userdata->user_login ); ?>" class="settings-input"  disabled="disabled"/>
+            <label for="new_user_name"><?php _e("New User name", "bp-username-changer") ?></label>
+            <input type="text" name="new_user_name" id="new_user_name" value="" class="settings-input" />
+            <p><?php _e("Enter the new Username of your choice", "bp-username-changer") ?></p>
+            <p class="submit"><input type="submit" id="change_username_submit" name="change_username_submit" class="button" value="<?php _e('Save Changes', 'bp-username-changer') ?>" /></p>
 
 	</form>
 
-	<?php
-}
+    <?php
+    
+    }
 
-/**
- * Settings content title
- */
-function bpdev_bpcu_title() {
+    /**
+    * Settings content title
+    */
+    public function print_title() {
 
-	echo '<h3>' . __( 'Change Username', 'bpcu' ) . '</h3>';
-}
+	echo '<h3>' . __( 'Change Username', 'bp-username-changer' ) . '</h3>';
 
-/**
- * Check if Username is reserved
- *
- * @param type $username
- * @return boolean
- */
-function bpdev_bpcu_is_reserved_name( $username ) {
+    }
+
+    /**
+    * Check if Username is reserved
+    *
+    * @param type $username
+    * @return boolean
+    */
+    public function is_reserved_name( $username ) {
 
 	$reserved = array();
 
 	$admin_names = array( 'admin', 'administrator' );
 
-	if ( is_super_admin() && in_array( $username, $admin_names ) )
-		return false; //do not prohibit the super admin from any username
-
-//other than that, check for all illigal names
+        if ( is_super_admin() && in_array( $username, $admin_names ) ){
+            
+            return false; //do not prohibit the super admin from any username
+            
+        }
+		
+        //other than that, check for all illigal names
 	if ( function_exists( 'bp_core_get_illegal_names' ) ) {
 
-		$reserved = bp_core_get_illegal_names();
+            $reserved = bp_core_get_illegal_names();
 
+	} elseif ( function_exists( 'bp_core_illegal_names' ) ) {
 
-	}elseif ( function_exists( 'bp_core_illegal_names' ) ) {
-
-		$reserved = bp_core_illegal_names();
+            $reserved = bp_core_illegal_names();
 
 	}
 
-	if ( in_array( $username, (array) $reserved ) )
-		return true;
-
+        if ( in_array( $username, (array) $reserved ) ){
+            
+            return true;
+            
+        }
+		
 	return false;
+    
+    }
+
 }
+BP_Username_Change_Helper::get_instance();
